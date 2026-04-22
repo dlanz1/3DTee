77bd084a76d2a64ef404d4322c4fe748c31886eb
TrevorLeung05 <trevor.leung7375@gmail.com>
2026-04-22 10:16:39 -0700

    did parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,12 @@
+import fs from "node:fs"
+import ohm from "ohm-js"
+
+const grammar = ohm.grammar(fs.readFileSync(new URL("./3DTee.ohm", import.meta.url), "utf-8"))
+
 export default function parse(sourceCode) {
-    throw new Error("Not implemented yet")
+    const match = grammar.match(sourceCode)
+    if (match.succeeded()) {
+        return match
+    }
+    throw new Error(match.message)
 }
